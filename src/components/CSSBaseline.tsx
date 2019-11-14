d45942be2dd8b20b8589ba1faf3277a9b223728a
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-11-14 09:59:45 -0800

    added a css baseline component, nice to reset default styles and stuff

---
+++ src/components/CSSBaseline.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './CSSBaseline.scss';
+
+export default class CSSBaseline extends React.Component {
+  public render() {
+    const { children = null } = this.props;
+
+    return <>{children}</>;
+  }
+}
