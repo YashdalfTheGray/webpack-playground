d5f37774d3e062e4a953f8b793cf4824e126fe86
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2019-11-14 09:59:16 -0800

    added an app header component

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import './AppHeader.scss';
+
+interface IAppHeaderProps {
+  name: string;
+}
+
+export default class AppHeader extends React.Component<IAppHeaderProps> {
+  public render() {
+    const { name } = this.props;
+    return (
+      <div className="AppHeader">
+        <h1>{name}</h1>
+      </div>
+    );
+  }
+}
